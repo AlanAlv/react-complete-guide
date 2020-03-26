69b5a37375a85065bae381f68a3c8e695916e33b
AlanAlv <alan.alv96@gmail.com>
2020-03-25 23:17:17 -0600

    Handling Dynamic Content

--- src/App.js
+++ src/App.js
@@ -49,17 +49,11 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-          style={style} 
-          onClick={this.togglePersonsHandler}>
-            Toggle Persons
-        </button>
-        {this.state.showPersons === true ? 
-          <div>
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
           <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age} />
@@ -71,8 +65,21 @@ class App extends Component {
           <Person 
             name={this.state.persons[2].name} 
             age={this.state.persons[2].age} />
-          </div> : null
-        }
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style} 
+          onClick={this.togglePersonsHandler}>
+            Toggle Persons
+        </button>
+        {persons}
+
       </div>
     );
   }
